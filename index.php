90321ec426a9b21416a09c0fe4d3a99a6dec64ad
Ali Malik <ialimalik99@gmail.com>
2021-04-03 10:53:17 -0400

    Add jQuery

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
 <html>
 
 <head>
+    <meta name="viewport" content="initial-scale = 1.0, user-scalable = no">
+    <meta charset="utf-8">
+    <title> My Shopping App</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <style>
 
+    </style>
 </head>
+</body>
+
+</body>
 
 </html>
